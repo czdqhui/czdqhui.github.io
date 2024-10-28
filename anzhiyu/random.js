4222e3dfe036b88c3380319c1c40766d1d683065
duanqihui <diaomao3342207867@163.com>
2024-10-28 18:59:45 +0800

    Site updated: 2024-10-28 18:59:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/21/hello-world/"];function toRandomPost(){
+var posts=["2024/10/17/duanlian/","2024/08/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
